d4a56cd19cc8da5aa04124fb9fc181f4b1ea98d9
alexandercyu <alexanderc.yu@utoronto.ca>
2024-10-17 17:06:02 -0400

    Create ui-styles-scripts.js
    Add functions for changing placeholder text in search box

---
+++ templates/site/js/ui-styles-scripts.js
@@ -0,0 +1,8 @@
+function setPlaceholderText(){
+    var searchInputElement = document.getElementById("searchInput");
+    searchInputElement.setAttribute("placeholder", "Search by keyword");
+}
+function updatePlaceholderText(){
+    var searchInputElement = document.getElementById("searchInput");
+    searchInputElement.setAttribute("placeholder", "Type to search");
+}
