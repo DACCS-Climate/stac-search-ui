33c0da2022a5a5ee1125eb243722dfbe82e2f455
alexandercyu <alexanderc.yu@utoronto.ca>
2024-12-18 15:20:27 -0500

    Delete datepicker.js
    Have script directly in datepicker.html

--- templates/site/js/datepicker.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    $( "#inputDateReg" ).datepicker({
-        showOtherMonths: true,
-        selectOtherMonths: true,
-        dateFormat: "yy mm dd"
-    });
-
-});
