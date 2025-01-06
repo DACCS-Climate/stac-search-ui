48fad867f1fcee12a1113ae7692c51ad24ad5def
alexandercyu <alexanderc.yu@utoronto.ca>
2025-01-06 17:11:04 -0500

    Update ui-styles-scripts.js
    - Modify chevron rotation functions to use the supplied element ID and css class
    - Add function to toggle text in Source dropdown and display stac version.  Note this does not work on localhost and uses the Magpie API

--- templates/site/js/ui-styles-scripts.js
+++ templates/site/js/ui-styles-scripts.js
@@ -1,11 +1,11 @@
-function toggleChevronRotate(elementID){
+function toggleChevronRotate(elementID, className){
     var dropdownTitle = document.getElementById(elementID)
 
-    if(dropdownTitle.classList.contains("dropdown-panel-icon")){
-        dropdownTitle.classList.remove("dropdown-panel-icon");
+    if(dropdownTitle.classList.contains(className)){
+        dropdownTitle.classList.remove(className);
     }
     else{
-        dropdownTitle.classList.add("dropdown-panel-icon");
+        dropdownTitle.classList.add(className);
     }
 
 }
@@ -13,7 +13,7 @@ function toggleChevronRotate(elementID){
 function removeChevronRotate(elementID){
     var dropdownTitle = document.getElementById(elementID)
 
-    dropdownTitle.classList.remove("dropdown-panel-icon");
+    dropdownTitle.classList.remove(className);
 
 }
 
@@ -26,6 +26,52 @@ function toggleDropdownPanelBackground(elementID){
     else{
         dropdownTitle.classList.add("dropdown-panel-title-white");
     }
+}
+
+function toggleTitleText(){
+    var titleText = document.getElementById("dropdownTitleText");
+    var titleStacVersionDiv = document.getElementById("dropdownStacTitle");
+
+    if(titleText.classList.contains("display-none")){
+        titleText.classList.remove("display-none");
+    }
+    else{
+        titleText.classList.add("display-none");
+    }
+
+    if(titleStacVersionDiv.classList.contains("display-none")){
+        titleStacVersionDiv.classList.remove("display-none");
+    }
+    else{
+        titleStacVersionDiv.classList.add("display-none");
+    }
+
+}
+
+function populateStacVersion(elementID){
+    var titleStacVersionDiv = document.getElementById(elementID);
+
+    getNodeServices().then(json => {
+        Object.entries(json.services).forEach( ([index, service]) => {
+            if(service.name == "stac"){
+                titleStacVersionDiv.innerHTML = service.version;
+            }
+         })
+
+     });
+}
+
+async function getNodeServices(){
+    const servicesURLFragment = "/services";
+
+    const response = await fetch(servicesURLFragment, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json, text/plain",
+            "Content-Type": "application/json"
+        }
+    });
+    return await response.json();
 }
 
 function removeDropdownPanelBackground(elementID){
