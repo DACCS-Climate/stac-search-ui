3fbc85acc45be64133de84609e6029b42301b633
alexandercyu <alexanderc.yu@utoronto.ca>
2025-01-10 17:39:00 -0500

    Create dropdown-list.js
    Create function to swap or replace values on list item click event

---
+++ templates/site/js/dropdown-list.js
@@ -0,0 +1,18 @@
+function swapListItems(firstItemID, secondItemID){
+    var firstItem = document.getElementById(firstItemID);
+    var secondItem = document.getElementById(secondItemID);
+    
+    var firstItemValue = firstItem.innerHTML;
+
+    firstItem.innerHTML = secondItem.innerHTML;
+    secondItem.innerHTML = firstItemValue;
+}
+
+function replaceListItem(placeholderValue, firstItemID, secondItemID){
+    var firstItem = document.getElementById(firstItemID);
+    var secondItem = document.getElementById(secondItemID);
+
+    var firstItemValue = firstItem.innerHTML;
+
+    firstItem.innerHTML = secondItem.innerHTML;
+}
