dcbd80dc97a16d49b68d862fc6326bc2b8823b3a
alexandercyu <alexanderc.yu@utoronto.ca>
2025-01-10 16:16:04 -0500

    Update sample.js
    - moved dropdown script to dropdown.html template
    - moved spin button script to spin button template

--- templates/site/js/sample.js
+++ templates/site/js/sample.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-    //setDropdownItemRemoveChevron();
-
     //Set placeholder text for the following elements
 
     //For search input box
@@ -38,41 +36,4 @@ document.addEventListener("DOMContentLoaded", function (){
         setPlaceholderText(textInputElement , "1");
         setTextboxBackground(textInputElement);
     });
-
-    //For Spin button
-    var spinButtonUp = document.getElementById("spinButtonUp");
-    spinButtonUp.addEventListener("click", function(){
-        increment("inputNumberSmall");
-    });
-
-    var spinButtonDown = document.getElementById("spinButtonDown");
-    spinButtonDown.addEventListener("click", function(){
-        decrement("inputNumberSmall");
-    });
-
-    var dropdownDefaultList = document.getElementById("dropdownListDefaultLabel");
-    dropdownDefaultList.addEventListener("click", function() {
-        toggleDropdownStyles("dropdownListDefaultContainer", "dropdown-transition-styles");
-        //toggleDropdownStyles("dropdownListDefaultLabel", "dropdown-default-list-label-toggle");
-        //toggleDropdownStyles("dropdownListDefaultChevron", "dropdown-chevron-rotate");
-    });
-
-    var dropdownRegularListMetadata = document.getElementById("dropdownListRegularMetadataLabel");
-    dropdownRegularListMetadata.addEventListener("click", function() {
-        toggleDropdownStyles("dropdownListRegularMetadataContainer", "dropdown-transition-styles");
-        toggleDropdownStyles("dropdownListRegularMetadataChevron", "dropdown-chevron-rotate");
-    });
-
-    var dropdownRegularListLicense = document.getElementById("dropdownListRegularLicenseLabel");
-    dropdownRegularListLicense.addEventListener("click", function() {
-        toggleDropdownStyles("dropdownListRegularLicenseContainer", "dropdown-transition-styles");
-        toggleDropdownStyles("dropdownListRegularLicenseChevron", "dropdown-chevron-rotate");
-    });
-
-/*
-    var dropdownRegularShare = document.getElementById("dropdownRegularShareLabel");
-    dropdownRegularShare.addEventListener("click", function() {
-        toggleDropdownStyles("dropdownRegularShareContainer", "dropdown-regular-container-transition");
-        toggleDropdownStyles("dropdownRegularShareChevron", "dropdown-chevron-rotate");
-    });*/
 })
