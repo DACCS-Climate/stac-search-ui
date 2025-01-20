6d52c422a6c0a1dafcb9d2a9d5125ba0ef119c8c
alexandercyu <alexanderc.yu@utoronto.ca>
2025-01-20 17:18:54 -0500

    Update spinbutton.js
    background colour of number input remains white when there are values in it

--- templates/site/js/spinbutton.js
+++ templates/site/js/spinbutton.js
@@ -11,3 +11,14 @@ function decrement(targetElementID){
     var newValue = Number(target.value) - 1;
     target.value = newValue;
 }
+
+function changeInputBGColour(targetElementID){
+    var inputField = document.getElementById(targetElementID);
+
+    if(inputField.value != ""){
+        inputField.classList.add("input-number-small-background");
+    }
+    else{
+        inputField.classList.remove("input-number-small-background");
+    }
+}
