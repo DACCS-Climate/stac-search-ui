1c8f719144c046c2c59c933e2edde1b1ae71fc91
alexandercyu <alexanderc.yu@utoronto.ca>
2025-01-21 16:31:31 -0500

    Update spinbutton.js
    Remove function to change background in small number input

--- templates/site/js/spinbutton.js
+++ templates/site/js/spinbutton.js
@@ -11,14 +11,3 @@ function decrement(targetElementID){
     var newValue = Number(target.value) - 1;
     target.value = newValue;
 }
-
-function changeInputBGColour(targetElementID){
-    var inputField = document.getElementById(targetElementID);
-
-    if(inputField.value != ""){
-        inputField.classList.add("input-number-small-background");
-    }
-    else{
-        inputField.classList.remove("input-number-small-background");
-    }
-}
