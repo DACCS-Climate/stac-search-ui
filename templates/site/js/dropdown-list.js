9e22b08d56d31e67c05f63cc249fd067ba0b7d28
alexandercyu <alexanderc.yu@utoronto.ca>
2025-01-22 16:33:53 -0500

    Update dropdown-list.js
    Add function to make bottom border of the first item in the small and medium dropdown lists square when they are clicked

--- templates/site/js/dropdown-list.js
+++ templates/site/js/dropdown-list.js
@@ -1,3 +1,6 @@
+
+
+
 function swapListItems(firstItemID, secondItemID){
     var firstItem = document.getElementById(firstItemID);
     var secondItem = document.getElementById(secondItemID);
@@ -12,7 +15,18 @@ function replaceListItem(placeholderValue, firstItemID, secondItemID){
     var firstItem = document.getElementById(firstItemID);
     var secondItem = document.getElementById(secondItemID);
 
-    var firstItemValue = firstItem.innerHTML;
-
     firstItem.innerHTML = secondItem.innerHTML;
+    firstItem.classList.remove("bottom-no-border-radius");
+}
+
+function swapBorderRadius(firstListItemID){
+    var firstListItem = document.getElementById(firstListItemID);
+    var ariaExpanded = firstListItem.getAttribute("aria-expanded");
+
+    if (ariaExpanded == "true"){
+        firstListItem.classList.add("bottom-no-border-radius");
+    }
+    else{
+        firstListItem.classList.remove("bottom-no-border-radius");
+    }
 }
