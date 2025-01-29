7a23dd0d8fd79eedaf257184386628fe38174781
alexandercyu <alexanderc.yu@utoronto.ca>
2025-01-29 16:44:56 -0500

    Update leaflet-stac-ui.js
    - Move createDrawMenu() and addSearch() to leaflet-ui-functions.js and run it from the createMap() function
    
    - change map variable name
    - comment out getLayers Button
    - Add function to add marker to map when user manually enters latitude and longitude coordinates

--- templates/site/js/leaflet-stac-ui.js
+++ templates/site/js/leaflet-stac-ui.js
@@ -1,27 +1,32 @@
 
 document.addEventListener("DOMContentLoaded", function (){
-    var map = createMap("Toronto");
+    var newMap = createMap("Toronto");
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-//layeradd
+    var pointTextInput = document.getElementById("inputPointLatLng");
+
+    pointTextInput.addEventListener('keydown', function(event){
+        if(event.key == "Enter"){
+            addPoint("inputPointLatLng", newMap);
+        }
+    });
 
     //Adds drawing menu
-    createDrawMenu(map);
+    //createDrawMenu(newMap);
 
     //Uploads geoJSON pasted into textarea
     var uploadGeoJSONButton = document.getElementById("uploadGeoJSON");
     uploadGeoJSONButton.addEventListener("click", function(){
-        uploadGeoJSON(map);
+        uploadGeoJSON(newMap);
     });
 
 
     //Adds search box to map
-    addSearch(map);
-
+    //addSearch(newMap);
+/*
     var getLayersButton = document.getElementById("layerButton");
     getLayersButton.addEventListener("click", function(){
-        getLayers(map);
-    });
+        getLayers(newMap);
+    });*/
 
 })
 
