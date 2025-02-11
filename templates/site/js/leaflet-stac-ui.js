c57b92a9854712976f62fa792e0ff20bd2791839
alexandercyu <alexanderc.yu@utoronto.ca>
2025-02-11 18:06:43 -0500

    Update leaflet-stac-ui.js
    Remove unneeded code

--- templates/site/js/leaflet-stac-ui.js
+++ templates/site/js/leaflet-stac-ui.js
@@ -1,22 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (){
-    var newMap = createMap("Toronto");
-
-    //Keep in case coordinate point input field gets put back outside of map
-/*
-    var pointTextInput = document.getElementById("inputPointLatLng");
-
-    pointTextInput.addEventListener('keydown', function(event){
-        if(event.key == "Enter"){
-            addCoordinate("inputPointLatLng", newMap);
-        }
-    });*/
-
-
-    //Uploads geoJSON pasted into textarea
-    var uploadGeoJSONButton = document.getElementById("uploadGeoJSON");
-    uploadGeoJSONButton.addEventListener("click", function(){
-        uploadGeoJSON(newMap);
-    });
+    //var newMap = createMap("Toronto");
+    createMap("Toronto");
 })
 
 
