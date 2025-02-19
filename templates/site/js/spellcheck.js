3a5f26ebfe56e9f17d8fd746070f0aaea8fe7158
alexandercyu <alexanderc.yu@utoronto.ca>
2025-02-19 16:03:49 -0500

    Update spellcheck.js
    Removed commented line

--- templates/site/js/spellcheck.js
+++ templates/site/js/spellcheck.js
@@ -1,7 +1,3 @@
-//Used for overriding the Typo.js dictionary definition
-//var dictionary = new Typo(`{{ custom_typojs_dictionary }}`, false, false, { dictionaryPath: "dictionary" })
-
-
 function getWord(elementID){
     var inputBox = document.getElementById(elementID);
     var textInput = inputBox.value;
