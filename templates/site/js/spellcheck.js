91025bd1ff0d155a7207221081f35eec85a85cf9
alexandercyu <alexanderc.yu@utoronto.ca>
2025-02-19 16:42:39 -0500

    Delete spellcheck.js
    Remove Typo.js  function because Fuse search suits needs better

--- templates/site/js/spellcheck.js
+++
@@ -1,8 +0,0 @@
-function getWord(elementID){
-    var inputBox = document.getElementById(elementID);
-    var textInput = inputBox.value;
-    var suggestedWord = dictionary.suggest(textInput);
-    var wordOutput = document.getElementById("suggestedWordOutput");
-
-    wordOutput.innerText = suggestedWord;
-}
