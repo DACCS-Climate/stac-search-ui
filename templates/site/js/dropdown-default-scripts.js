ea2f04adb20b02a342fbf3788867ac302cd812bd
alexandercyu <alexanderc.yu@utoronto.ca>
2025-02-21 13:08:16 -0500

    Update dropdown-default-scripts.js
    Add function for a checkbox to check all checkboxes in the dropdown list and display how many are checked in the title of the dropdown.

--- templates/site/js/dropdown-default-scripts.js
+++ templates/site/js/dropdown-default-scripts.js
@@ -18,3 +18,34 @@ function checkCheckboxCount( listULID, defaultDropdownButtonTextID, defaultDropd
         dropdownButtonTextElement.innerText = checkboxCount + " Catalogs Selected" ;
     }
 }
+
+function selectAllCheckbox(listULID, defaultDropdownButtonTextID, defaultDropdownLabelText){
+    var checkboxCount = 0;
+    var dropdownButtonTextElement = document.getElementById(defaultDropdownButtonTextID);
+    var list = document.getElementById(listULID);
+    var checkboxArray = list.querySelectorAll('input[type=checkbox]')
+
+    if(checkboxArray[0].checked == true){
+        for(checkbox of checkboxArray){
+            checkbox.checked = true
+            if(checkbox.checked){
+                checkboxCount = checkboxCount + 1;
+            }
+        }
+
+        checkboxCount = checkboxCount - 1;
+
+        if(checkboxCount == 0){
+            dropdownButtonTextElement.innerText = defaultDropdownLabelText ;
+        }
+        else{
+            dropdownButtonTextElement.innerText = checkboxCount + " Catalogs Selected" ;
+        }
+    }
+    else{
+        for(checkbox of checkboxArray){
+            checkbox.checked = false;
+        }
+        dropdownButtonTextElement.innerText = defaultDropdownLabelText ;
+    }
+}
