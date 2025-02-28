b124a6604a3c31b56c8c81a86535cad9039c5dc7
alexandercyu <alexanderc.yu@utoronto.ca>
2025-02-28 18:46:41 -0500

    Create catalog-search.js
    - Copy code over from Misha's fuse search demo
    - Add code to build list elements to display result
    - Add function to clear list every time a new search term is entered
    - Add code to swap aria-expanded status so it can be used in CSS for styles

---
+++ templates/site/js/catalog-search.js
@@ -0,0 +1,75 @@
+async function getWordlist() {
+    const resp = await fetch("{{ stac_catalog_url }}/collections")
+    const json = await resp.json()
+    const collection_ids = json.collections.map(collection => collection.id)
+    return await Promise.all(collection_ids.map(async (id) => {
+        const resp_1 = await fetch(`{{ stac_catalog_url }}/collections/${id}/queryables`)
+        const json_1 = await resp_1.json()
+        return Object.entries(json_1.properties).map(([key, val]) => {
+            val["key"] = key
+            return val
+        })
+    })).then(queryables => queryables.flat())
+}
+
+let fuse = null;
+
+function makeFuse() {
+    getWordlist().then(queryables => {
+        fuse = new Fuse(queryables, {
+            keys: ["title", "key", "enum"],
+            threshold: 0.2, // TODO: experiment with threshold and distance values to get best results
+            distance: 10,
+            includeMatches: true
+        });
+        console.log("search is ready") // TODO: replace this with some proper message to the user
+    })
+}
+
+makeFuse()
+
+const wordOutput = document.getElementById("suggestedWordOutput");
+
+function clearListChildren(){
+    const element = document.getElementById("suggestedWordOutputList");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function getWord(inputBox){
+    var queryablesArray = [];
+    var queryableResultButton;
+    var queryResultList = document.getElementById("suggestedWordOutputList");
+
+    inputBox.setAttribute("aria-expanded", "true");
+
+    if (fuse !== null) {
+
+        queryablesArray = fuse.search(inputBox.value).map(obj => obj.matches.map(match => match.value).flat()).flat();
+        for (item of queryablesArray){
+            var queryResultListItem = document.createElement("li");
+            queryableResultButton = document.createElement('a');
+            queryableResultButton.setAttribute('role', 'button');
+            queryableResultButton.id = item.toLowerCase() + queryablesArray.indexOf(item) ;
+            queryableResultButton.innerText = item;
+
+            queryableResultButton.addEventListener('click', function(event){
+                console.log(event.target.id);
+                collapseSearchResults(inputBox, event.target.id);
+            })
+
+            queryResultListItem.appendChild(queryableResultButton);
+            queryResultList.appendChild(queryResultListItem);
+
+        }
+    }
+}
+
+function collapseSearchResults(inputBox, buttonID){
+    var listButton = document.getElementById(buttonID);
+    inputBox.setAttribute("aria-expanded", "false");
+    inputBox.value = listButton.innerText;
+
+
+}
