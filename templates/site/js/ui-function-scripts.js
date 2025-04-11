46d373067b029fd90d2b82b458d22e070fbd5de2
alexandercyu <alexanderc.yu@utoronto.ca>
2025-04-11 17:56:35 -0400

    Update ui-function-scripts.js
    Add functionality to the Back button to hide the dataset details and show the search results

--- templates/site/js/ui-function-scripts.js
+++ templates/site/js/ui-function-scripts.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function (){
+    var backButton = document.getElementById("backButton");
+    backButton.addEventListener("click", function() {
+        swapDatasetDetails();
+    });
+})
+
 function stopPropagation(event){
     event.stopPropagation();
 }
