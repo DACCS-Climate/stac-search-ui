4c4228ba6d2ec16b00ca84f22e68e7e3d24c81f3
alexandercyu <alexanderc.yu@utoronto.ca>
2025-04-21 15:18:05 -0400

    Update dropdown-default-scripts.js
    - Add function to return STAC json so it can be used by other functions without multiple fetch functions
    - expand function that builds the catalog filter to all checkboxes in the dropdown list
    -  start functions to build filter for Frequency dropdown

--- templates/site/js/dropdown-default-scripts.js
+++ templates/site/js/dropdown-default-scripts.js
@@ -1,10 +1,26 @@
+async function getSTACJSON(){
+    var productionURL = "{{ stac_catalog_url }}/search?";
+    var testingURL = "https://infomatics-dcs.cs.toronto.edu/stac/search?";
+    var stacSearchURL;
+    var queryParams = "";
+
+    const response = await fetch(testingURL, {
+        method: "GET",
+        headers: {
+                "Accept": "application/json, text/plain",
+                "Content-Type": "application/json"
+            }
+    });
+
+    return await response.json();
+}
+
 function setSearchPageTitle(listULID, titleDivID){
     var titleDiv = document.getElementById(titleDivID);
     var list = document.getElementById(listULID);
     var dynamicTitle = "";
     var checkboxCount = 0;
-    var checkboxArray = list.querySelectorAll('input[type=checkbox]')
-    console.log(checkboxArray);
+    var checkboxArray = list.querySelectorAll('input[type=checkbox]');
 
      if(checkboxArray[0].checked == true){
         checkboxCount = -1;
@@ -38,7 +54,7 @@ function checkCheckboxCount( listULID, defaultDropdownButtonTextID, defaultDropd
     var checkboxCount = 0;
 
     var list = document.getElementById(listULID);
-    var checkboxArray = list.querySelectorAll('input[type=checkbox]')
+    var checkboxArray = list.querySelectorAll('input[type=checkbox]');
 
     if(checkboxArray[0].checked == true){
         checkboxCount = -1;
@@ -68,7 +84,7 @@ function selectAllCheckbox(listULID, defaultDropdownButtonTextID, defaultDropdow
     var checkboxCount = 0;
     var dropdownButtonTextElement = document.getElementById(defaultDropdownButtonTextID);
     var list = document.getElementById(listULID);
-    var checkboxArray = list.querySelectorAll('input[type=checkbox]')
+    var checkboxArray = list.querySelectorAll('input[type=checkbox]');
 
     if(checkboxArray[0].checked == true){
         for(checkbox of checkboxArray){
@@ -93,4 +109,55 @@ function selectAllCheckbox(listULID, defaultDropdownButtonTextID, defaultDropdow
         }
         dropdownButtonTextElement.innerText = defaultDropdownLabelText ;
     }
+}
+
+function setCheckboxFilterJSON(listULID){
+    var searchFilterDatasetsHidden = document.getElementById("searchFilterDatasetsHidden");
+    var list = document.getElementById(listULID);
+    var checkboxArray = list.querySelectorAll('input[type=checkbox]');
+    var argumentArray = [];
+    var argumentJSON = JSON.parse('{"op": "or","args" : [ ]}');
+    var argumentObject;
+
+    if(checkboxArray[0].checked == true) {
+        for(let i = 1; i < checkboxArray.length; i++){
+            argumentObject = {'op': '=','args' : [{ 'property': 'collection' },  checkboxArray[i].value ]};
+            argumentArray.push(argumentObject);
+        }
+    }
+    else{
+        for (checkbox of checkboxArray) {
+            if(checkbox.checked){
+                argumentObject = {'op': '=','args' : [{ 'property': 'collection' }, checkbox.value ]};
+                argumentArray.push(argumentObject);
+            }
+        }
+    }
+
+    argumentJSON["args"] = argumentArray;
+    searchFilterDatasetsHidden.innerText = JSON.stringify(argumentJSON);
+}
+
+function populateDatasetsFilter(listULID){
+    var list = document.getElementById(listULID);
+    var checkboxArray = list.querySelectorAll('input[type=checkbox]');
+
+
+}
+
+function populateFrequencyFilter(listULID){
+
+
+}
+
+
+function setFrequencyFilter(frequencyListItemID){
+    var listItem = document.getElementById(frequencyListItemID);
+    var stacJSON = getSTACJSON();
+
+    var license;
+
+
+
+
 }
