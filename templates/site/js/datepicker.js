97e49400f2316f151def6657b66507ea257591b3
alexandercyu <alexanderc.yu@utoronto.ca>
2025-04-23 17:05:30 -0400

    Update datepicker.js
    - make flatpickr initialize on user supplied HTML element instead of fixed element
    - add function to set end date and year of calendar to current end of year

--- templates/site/js/datepicker.js
+++ templates/site/js/datepicker.js
@@ -1,11 +1,19 @@
 function initializeCalendar(datepickerID, startDate, endDate){
     var datepickerElement = document.getElementById(datepickerID);
-        flatpickr(datepickerElement, {
-            dateFormat: "Y M d",
-            shorthandCurrentMonth: true,
-            allowInput: true,
-            monthSelectorType: "dropdown",
-            minDate: startDate,
-            maxDate: endDate
-        });
-    }
+    flatpickr(datepickerElement, {
+        dateFormat: "Y M d",
+        shorthandCurrentMonth: true,
+        allowInput: true,
+        monthSelectorType: "dropdown",
+        minDate: startDate,
+        maxDate: endDate
+    });
+}
+
+function getEndDateString(){
+    var currentDate = new Date();
+    var nextYear = currentDate.getFullYear() + 1;
+    var currentDateString = "12" + " " + "31" + " " + nextYear.toString();
+
+    return currentDateString;
+}
