111b3913eb3d57892f787e57895a1dea9c7af061
alexandercyu <alexanderc.yu@utoronto.ca>
2025-05-22 15:56:13 -0400

    Update leaflet-stac-ui.js
    - Add functions to show modal with Leaflet map in it
    - Move the map instantiation for the modal inside the button click so it loads the map after the modal is shown (fixes error where map loads background images according to size of containing div)

--- templates/site/js/leaflet-stac-ui.js
+++ templates/site/js/leaflet-stac-ui.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", function (){
+
+    var showLocationMapButton = document.getElementById("showLocationMap");
+
+    showLocationMapButton.addEventListener('click', function(){
+        var mapModal = document.getElementById("modalLeafletMap");
+        mapModal.showModal();
+
+        var mapModalInstance = instantiateMap("modalMap");
+        createMap(mapModalInstance, true);
+    });
+
+
+    var closeMapModalButton = document.getElementById("modalMapClose");
+    closeMapModalButton.addEventListener('click', function(){
+        var mapModal = document.getElementById("modalLeafletMap");
+        mapModal.close();
+    });
+
+    /*Create Leaflet map instance for dataset details*/
     var mapInstance = instantiateMap("datasetMapContainer");
     createMap(mapInstance, false);
 })
